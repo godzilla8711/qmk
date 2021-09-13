d940a42cef613f6d84c203b42df01c6394192ba1
Tony Cetuk <cetukt@gmail.com>
2021-09-13 17:54:57 -0400

    Added compilation script

--- compile.sh
+++ compile.sh
@@ -3,16 +3,16 @@
 KEYBOARD_HEX_FOLDER=/mnt/c/keyboards
 
 # Compile kbd67...
- qmk compile -kb kbdfans/kbd67/rev2 -km tony
- cp kbdfans_kbd67_rev2_tony.hex $KEYBOARD_HEX_FOLDER
+qmk compile -kb kbdfans/kbd67/rev2 -km tony
+cp kbdfans_kbd67_rev2_tony.hex $KEYBOARD_HEX_FOLDER
 
 # Compile kbd67 split spacebar...
- qmk compile -kb kbdfans/kbd67/rev2 -km tony_split_spacebar
- cp kbdfans_kbd67_rev2_tony_split_spacebar.hex $KEYBOARD_HEX_FOLDER
+qmk compile -kb kbdfans/kbd67/rev2 -km tony_split_spacebar
+cp kbdfans_kbd67_rev2_tony_split_spacebar.hex $KEYBOARD_HEX_FOLDER
 
 # Compile dz60 WKL hot swap...
- qmk compile -kb dztech/dz60rgb_wkl/v2 -km tony60wkl
- cp dztech_dz60rgb_wkl_v2_tony60wkl.hex $KEYBOARD_HEX_FOLDER
+qmk compile -kb dztech/dz60rgb_wkl/v2 -km tony60wkl
+cp dztech_dz60rgb_wkl_v2_tony60wkl.hex $KEYBOARD_HEX_FOLDER
 
 # Compile kbd67 mark ii rgb v2
 qmk compile -kb kbdfans/kbd67/mkiirgb/v2 -km tony67mkii
@@ -20,4 +20,4 @@ cp kbdfans_kbd67_mkiirgb_v2_tony67mkii.hex $KEYBOARD_HEX_FOLDER
 
 # Compile kbd67 mark ii rgb v3
 qmk compile -kb kbdfans/kbd67/mkiirgb/v3 -km tony67mkii
-cp kbdfans_kbd67_mkiirgb_v3_tony67mkii.hex $KEYBOARD_HEX_FOLDER
+cp kbdfans_kbd67_mkiirgb_v3_tony67mkii.bin $KEYBOARD_HEX_FOLDER
---
+++ initial_setup.sh
@@ -0,0 +1,9 @@
+# Run this once for a new qmk_firmware installation from folder ~/qmk
+cp notes.txt ../qmk_firmware
+cp compile.sh ../qmk_firmware
+cp export-to-git.sh ../qmk_firmware
+cp -rf keyboards-kbdfans-kbd67-mkiirgb-keymaps-tony67mkii ~/qmk_firmware/keyboards/kbdfans/kbd67/mkiirgb/keymaps/tony67mkii
+cp -rf keyboards-dztech-dz60rgb_wkl-keymaps-tony60wkl ../qmk_firmware/keyboards/dztech/dz60rgb_wkl/keymaps/tony60wkl
+cp -rf keyboards-kbdfans-kbd67-rev2-keymaps-tony ../qmk_firmware/keyboards/kbdfans/kbd67/rev2/keymaps/tony
+cp -rf keyboards-kbdfans-kbd67-rev2-keymaps-tony_split_spacebar ../qmk_firmware/keyboards/kbdfans/kbd67/rev2/keymaps/tony_split_spacebar
+
