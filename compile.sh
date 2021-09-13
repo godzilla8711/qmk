41dfbc87694970526f1f759a704e6e7c96c921aa
Tony Cetuk <cetukt@gmail.com>
2021-09-13 11:43:44 -0400

    Added notes for kbd67 mark 2 RGB v3

--- compile.sh
+++ compile.sh
@@ -14,7 +14,10 @@ KEYBOARD_HEX_FOLDER=/mnt/c/keyboards
  qmk compile -kb dztech/dz60rgb_wkl/v2 -km tony60wkl
  cp dztech_dz60rgb_wkl_v2_tony60wkl.hex $KEYBOARD_HEX_FOLDER
 
-# Compile kbd67 mark ii rgb
+# Compile kbd67 mark ii rgb v2
 qmk compile -kb kbdfans/kbd67/mkiirgb/v2 -km tony67mkii
 cp kbdfans_kbd67_mkiirgb_v2_tony67mkii.hex $KEYBOARD_HEX_FOLDER
 
+# Compile kbd67 mark ii rgb v3
+qmk compile -kb kbdfans/kbd67/mkiirgb/v3 -km tony67mkii
+cp kbdfans_kbd67_mkiirgb_v3_tony67mkii.hex $KEYBOARD_HEX_FOLDER
