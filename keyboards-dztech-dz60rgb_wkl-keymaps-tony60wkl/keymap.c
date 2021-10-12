24cba558840a73509e8bd9197c1ddb612fffa31c
Tony Cetuk <cetukt@gmail.com>
2021-10-12 10:39:46 -0400

    Updated spacebar to act as shift in control mode

--- keyboards-dztech-dz60rgb_wkl-keymaps-tony60wkl/keymap.c
+++ keyboards-dztech-dz60rgb_wkl-keymaps-tony60wkl/keymap.c
@@ -84,11 +84,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------------------------------------------------------------|
    * |     |   |   |   |    |   |   |   |   |   |PSc|   |   |      |
    * |-------------------------------------------------------------|
-   * |     |   |   |Shft|Bksp|Del|Lef|Dwn| Up|Rig|   |   |  Return |
+   * |     |   |   |    |Bksp|Del|Lef|Dwn| Up|Rig|   |   |  Return |
    * |-------------------------------------------------------------|
    * |        |   |   |   |   |   |Mac1|Mac2|   |   |  |     | PUp |
    * |-------------------------------------------------------------|
-   * |    |    |    |                       |       |       |      |
+   * |    |    |    |         Shft          |       |       |      |
    * `-------------------------------------------------------------'
    * Notes: { is KC_LCBR, } is KC_RCBR
    *        KC_TRNS use setting of same key from bottom layer
@@ -96,9 +96,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[1] = LAYOUT_60_tsangan_hhkb(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, KC_DEL, \
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PSCR, KC_NO,   KC_NO,   KC_NO,  \
-    KC_TRNS, KC_NO,   KC_NO,   KC_LSFT, KC_BSPC, KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,   KC_ENT,          \
+    KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_BSPC, KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,   KC_ENT,          \
     KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MAC_1,   MAC_2,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_PGUP,         \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,  KC_TRNS, KC_TRNS, KC_TRNS)
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_LSFT,  KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
 // Define macros
--- keyboards-kbdfans-kbd67-mkiirgb-keymaps-tony67mkii/keymap.c
+++ keyboards-kbdfans-kbd67-mkiirgb-keymaps-tony67mkii/keymap.c
@@ -70,11 +70,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |     |   |   |   |   |   |   |   |   |   |PSc|   |   |     |    |
    * |----------------------------------------------------------------|
-   * |     |   |   |Shft|Bksp|Del|Lef|Dwn| Up|Rig|   |   |Return  |   |
+   * |     |   |   |    |Bksp|Del|Lef|Dwn| Up|Rig|   |   |Return  |   |
    * |----------------------------------------------------------------|
    * |        |   |   |   |   |   |Mac1|Mac2|   |   |  |     |PUp|    |
    * |----------------------------------------------------------------|
-   * |    |    |    |        Space          |    |    |Hom |PDn |End  |
+   * |    |    |    |         Shft          |    |    |Hom |PDn |End  |
    * `----------------------------------------------------------------'
    * Notes: { is KC_LCBR, } is KC_RCBR
    *        KC_TRNS use setting of same key from bottom layer
@@ -82,9 +82,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[1] = LAYOUT_65_ansi_blocker(
       KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, KC_NO, \
       KC_TRNS, RGB_TOG, RGB_MOD, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PSCR, KC_NO,   KC_NO,   KC_NO,  KC_NO, \
-      KC_TRNS, KC_NO,   KC_NO,   KC_LSFT, KC_BSPC, KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,   KC_ENT,  KC_NO,         \
+      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_BSPC, KC_DEL,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,   KC_ENT,  KC_NO,         \
       KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   MAC_1,   MAC_2,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_PGUP, KC_NO,         \
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,  KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END)
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_LSFT,  KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END)
+     // KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,  KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_END)
 };
 
 // Define macros
